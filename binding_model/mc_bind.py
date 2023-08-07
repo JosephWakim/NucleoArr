c6a3420c3c6f96bcc3bb9556de44548e6bc3283b
JosephWakim <joewakim19@gmail.com>
2023-08-07 14:37:03 -0700

    updated binding simulation to fit sliding nucleosome framework

--- binding_model/mc_bind.py
+++ binding_model/mc_bind.py
@@ -15,16 +15,17 @@ from typing import Optional, List, Tuple, Dict
 import numpy as np
 from scipy.special import comb
 from sliding_nucleosome.mc import get_unique_simulation_name
+import sliding_nucleosome.nucleo_arr as nuc
 
 
-def accept_or_reject(dE: float, T: Optional[float] = 1) -> bool:
+def accept_or_reject(dE: float, Temp: Optional[float] = 1) -> bool:
     """Accept or reject an attempted MC move.
 
     Parameters
     ----------
     dE : float
         Energy change associated with the attempted move
-    T : Optional[float]
+    Temp : Optional[float]
         Temperature of the system (default = 1)
 
     Returns
@@ -35,11 +36,13 @@ def accept_or_reject(dE: float, T: Optional[float] = 1) -> bool:
     if dE <= 0:
         return True
     else:
-        return np.random.rand() <= np.exp(-dE / T)
+        return np.random.rand() <= np.exp(-dE / Temp)
 
 
 def update_state(
-    accept: bool, sigma: np.ndarray, sigma_new: np.ndarray,
+    accept: bool,
+    sigma: np.ndarray,
+    sigma_new: np.ndarray,
     inds: List[Tuple[int, int]]
 ) -> np.ndarray:
     """Update the state of the system to match that of an accepted move.
@@ -70,9 +73,11 @@ def update_state(
 
 
 def mc_N_state_1D(
-    m: np.ndarray, sigma: np.ndarray, B: np.ndarray, J: np.ndarray,
-    mu: np.ndarray, n_snaps: int, n_steps_per_snap: int,
-    Nbi: Optional[List[int]] = None, out_dir: Optional[str] = "output",
+    nuc_arr: nuc.NucleosomeArray,
+    sigma: np.ndarray,
+    n_snaps: int,
+    n_steps_per_snap: int,
+    out_dir: Optional[str] = "output",
     out_prefix: Optional[str] = "bind_snap_"
 ):
     """Simulate `Nr` mark and binder states on a 1D lattice.
@@ -83,29 +88,16 @@ def mc_N_state_1D(
 
     Parameters
     ----------
-    m : np.ndarray (N, Nm) of int
-        Modification state for each of Nm marks on each of N beads; elements
-        must be 0, 1, ..., Nr for the multistate model; here Nr is the total
-        number of sites on a bead that can express a particular mark and can be
-        bound by a binder
+    nuc_arr : nuc.NucleosomeArray
+        Nucleosome array object, which contains the linker lengths, modification
+        states, and physical properties of a 1D nucleosome array
     sigma : np.ndarray (N, Nm) of int
         Binding state for each of Nm binders on each of N beads; elements
         must be 0, 1, ..., Nr for the multistate model
-    B : np.ndarray (Nm, Nm) of float
-        Binding affinity for each combination of mark (axis 0) and binder (axis
-        1)
-    J : np.ndarray (Nm, Nm) of float
-        For each pair of binder types (i, j), the element J[i, j] indicates the
-        interaction energy gianed when binders of those types are nearby
-    mu : np.ndarray (Nm,) of float
-        Chemical potential of binders, biasing the binding affinity
     n_snaps : int
         Number of snapshots to save during the simulation
     n_steps_per_snap : int
         Number of Monte Carlo moves to attempt between savepoints
-    Nbi : Optional[List[int]]
-        Number of binders of type $i$ that can bind a single site; if None,
-        then each binder type can bind a single site one time.
     out_dir : Optional[str]
         Output directory into which snapshots will be saved (default = "Output")
     out_prefix : Optional[str]
@@ -116,31 +108,37 @@ def mc_N_state_1D(
     # Make the output directory
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
+
     # Make simulation output directory
-    sim_out_dir = get_unique_simulation_name(out_dir, "sim")
+    sim_out_dir = get_unique_simulation_name(out_dir, "bind_sim")
     sim_out_dir = os.path.join(out_dir, sim_out_dir)
     os.makedirs(sim_out_dir)
-    # Save the initial state
+
+    # Save the initial binding states
     np.savetxt(
         os.path.join(sim_out_dir, out_prefix + "init.csv"), sigma, fmt="%i",
         delimiter=","
     )
-    # Save the chemical potential
-    np.savetxt(
-        os.path.join(sim_out_dir, "mu.csv"), mu, fmt="%f",
-        delimiter=","
-    )
+
+    # Save the configuration of the nucleosome array
+    nuc_arr.save(os.path.join(sim_out_dir, "nuc_arr.csv"))
+
     # Run the simulation
     for snap in range(n_snaps):
         for ind in range(n_steps_per_snap):
+
             # Attempt an MC move
-            sigma_new, inds = binding_move_N_state_1D(sigma, Nbi)
+            sigma_new, inds = binding_move_N_state_1D(sigma, nuc_arr.Nbi)
+
             # Evaluate the energy change associated with the MC move
-            dE = dE_N_state_1D(m, sigma, sigma_new, inds, B, J, mu)
+            dE = dE_N_state_1D(nuc_arr, sigma, sigma_new, inds)
+
             # Accept or Reject the move
             accept = accept_or_reject(dE)
+
             # Update the state of the system
             sigma = update_state(accept, sigma, sigma_new, inds)
+
         # Save the current state
         np.savetxt(
             os.path.join(sim_out_dir, out_prefix + str(snap) + ".csv"), sigma,
@@ -151,7 +149,8 @@ def mc_N_state_1D(
 
 
 def binding_move_N_state_1D(
-    sigma: np.ndarray, Nbi: Optional[List[int]] = None
+    sigma: np.ndarray,
+    Nbi: Optional[List[int]] = None
 ) -> Tuple[np.ndarray, Tuple[int, int]]:
     """Attempt a change in Nr-binding states along a 1D lattice.
 
@@ -182,20 +181,25 @@ def binding_move_N_state_1D(
     """
     if Nbi is None:
         Nbi = [1 for _ in range(sigma.shape[1])]
+
     # Choose a bead and a binder on that bead
     bead_ind = np.random.randint(sigma.shape[0])
     binder_ind = np.random.randint(sigma.shape[1])
+
     # Choose a new binding state for that binder
     sigma_new = sigma.copy()
     sigma_new[bead_ind, binder_ind] = np.random.randint(Nbi[binder_ind]+1)
+
     # Return new binding state and the indices of the affected bead and binder
     inds = [(bead_ind, binder_ind)]
     return sigma_new, inds
 
 
 def dE_N_state_1D(
-    m: np.ndarray, sigma: np.ndarray, sigma_new: np.ndarray,
-    inds: Tuple[int, int], B: np.ndarray, J: np.ndarray, mu: np.ndarray
+    nuc_arr: nuc.NucleosomeArray,
+    sigma: np.ndarray,
+    sigma_new: np.ndarray,
+    inds: List[Tuple[int, int]]
 ) -> float:
     """Calculate the energy change associated with a binding move.
 
@@ -210,11 +214,9 @@ def dE_N_state_1D(
 
     Parameters
     ----------
-    m : np.ndarray (N, Nm) of int
-        Modification state for each of Nm marks on each of N beads; elements
-        must be 0, 1, ..., Nbi for the multistate model; here Nbi is the total
-        number of sites on a bead that can express a particular mark and can be
-        bound by a binder
+    nuc_arr : nuc.NucleosomeArray
+        Nucleosome array object, which contains the linker lengths, modification
+        states, and physical properties of a 1D nucleosome array
     sigma : np.ndarray (N, Nm) of int
         Binding state for each of Nm binders on each of N beads; elements
         must be 0, 1, ..., Nbi for the multistate model
@@ -224,14 +226,6 @@ def dE_N_state_1D(
     inds : List[Tuple[int, int]]
         List of tuples, each containing the bead index and the binder index
         affected by the attempted move in the form [(bead, binder), ...]
-    B : np.ndarray (Nm, Nm) of float
-        Binding affinity for each combination of mark (axis 0) and binder (axis
-        1)
-    J : np.ndarray (Nm, Nm) of float
-        For each pair of binder types (i, j), the element J[i, j] indicates the
-        interaction energy gianed when binders of those types are nearby
-    mu : np.ndarray (Nm,) of float
-        Chemical potential of binders, biasing the binding affinity
 
     Returns
     -------
@@ -246,50 +240,41 @@ def dE_N_state_1D(
             ind_left = [ind[0]-1, ind[1]]
         else:
             ind_left = [sigma.shape[0]-1, ind[1]]
-        dE += E_bead_N_state_1D(m, sigma_new, ind_left, B, J, mu)
-        dE -= E_bead_N_state_1D(m, sigma, ind_left, B, J, mu)
+        dE += E_bead_N_state_1D(nuc_arr, sigma_new, ind_left)
+        dE -= E_bead_N_state_1D(nuc_arr, sigma, ind_left)
 
         # Evaluate energy of the bond to the right of the affected site
-        dE += E_bead_N_state_1D(m, sigma_new, ind, B, J, mu)
-        dE -= E_bead_N_state_1D(m, sigma, ind, B, J, mu)
+        dE += E_bead_N_state_1D(nuc_arr, sigma_new, ind)
+        dE -= E_bead_N_state_1D(nuc_arr, sigma, ind)
 
     return dE
 
 
 def E_bead_N_state_1D(
-    m: np.ndarray, sigma: np.ndarray, ind: Tuple[int, int], B: np.ndarray,
-    J: np.ndarray, mu: np.ndarray
+    nuc_arr: nuc.NucleosomeArray,
+    sigma: np.ndarray,
+    ind: Tuple[int, int]
 ) -> float:
     """Calculate the energy of a single bead in its local environment.
 
     Notes
     -----
-    This function operates on the assumption that, on the same bead, readers
-    bind marked sites before they bind unmarked sites. The readers attain the
-    binding energy of all marked sites on the bead until the marked sites are
-    saturated.
+    There is some probability that the binder will bind to an unmarked site on
+    a nucleosome with marked sites. To account for this, we will use the
+    average binding energy over marked and unmarked sites, weighted by the
+    probability of binding to each site type.
 
     Parameters
     ----------
-    m : np.ndarray (N, Nm) of int
-        Modification state for each of Nm marks on each of N beads; elements
-        must be 0, 1, ..., Nbi for the multistate model; here Nbi is the total
-        number of sites on a bead that can express a particular mark and can be
-        bound by a binder
+    nuc_arr : nuc.NucleosomeArray
+        Nucleosome array object, which contains the linker lengths, modification
+        states, and physical properties of a 1D nucleosome array
     sigma : np.ndarray (N, Nm) of int
         Binding state for each of Nm binders on each of N beads; elements
         must be 0, 1, ..., Nbi for the multistate model
     ind : Tuple[int, int]
         Tuple containing the bead index and the binder index affected by the
         attempted move in the form (bead, binder)
-    B : np.ndarray (Nm, Nm) of float
-        Binding affinity for each combination of mark (axis 0) and binder (axis
-        1)
-    J : np.ndarray (Nm, Nm) of float
-        For each pair of binder types (i, j), the element J[i, j] indicates the
-        interaction energy gianed when binders of those types are nearby
-    mu : np.ndarray (Nm,) of float
-        Chemical potential of binders, biasing the binding affinity
 
     Returns
     -------
@@ -309,26 +294,34 @@ def E_bead_N_state_1D(
     E_neigh_int = 0
 
     # Binding energy
-    for i in range(n_binders):
-        E_bind += np.min([sigma[bead, i], m[bead, i]]) * B[i, i]
-        E_bind -= sigma[bead, i] * mu[i]
+    for binder in range(n_binders):
+        i = np.arange(sigma[bead, binder]+1)
+        weights = \
+            comb(nuc_arr.marks[bead, binder], i) * \
+            comb(
+                nuc_arr.Nbi[binder] - nuc_arr.marks[bead, binder],
+                sigma[bead, binder] - i
+            ) * (np.exp(-i * nuc_arr.B[binder, binder]))
+        denominator = np.sum(weights)
+        E_bind += np.sum(weights * i * nuc_arr.B[binder, binder]) / denominator
+        E_bind -= sigma[bead, binder] * nuc_arr.mu[binder]
 
     # Same-site, same-binder interactions
     for binder in range(n_binders):
-        E_local_int += comb(sigma[bead, binder], 2) * J[binder, binder]
+        E_local_int += comb(sigma[bead, binder], 2) * nuc_arr.J[binder, binder]
 
     # Same-site, different-binder interactions
     for binder_1 in range(n_binders - 1):
         for binder_2 in range(binder_1 + 1, n_binders):
             E_local_int += \
                 sigma[bead, binder_1] * sigma[bead, binder_2] * \
-                J[binder_1, binder_2]
+                nuc_arr.J[binder_1, binder_2]
 
     # Different-site interactions
     for binder_1 in range(n_binders):
         for binder_2 in range(n_binders):
             E_neigh_int += \
                 sigma[bead, binder_1] * sigma[bead_right, binder_2] * \
-                J[binder_1, binder_2]
+                nuc_arr.J[binder_1, binder_2] * nuc_arr.gamma[bead]
 
     return E_bind + E_local_int + E_neigh_int
